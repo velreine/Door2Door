b0bb8c41d720402712c9bc4109c15bef595c3d32
Jonas Iversen <Joi>
2022-03-04 09:51:49 +0100

    bulk commit

--- Door2Door-API/Controllers/Door2DoorController.cs
+++ Door2Door-API/Controllers/Door2DoorController.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Npgsql;
 using System.Data;
+using GeoJSON.Net;
+using NpgsqlTypes;
 
 namespace Door2Door_API.Controllers
 {
@@ -8,15 +10,18 @@ namespace Door2Door_API.Controllers
     [Route("[controller]")]
     public class Door2DoorController : ControllerBase
     {
-        private readonly string connectionString;
-
+        private readonly IFactory<Room> roomFactory;
+        // TODO: DELETE THIS!
+        private const string CONNECTIONSTRING =
+            "Server=192.168.1.102;Port=5432;Database=door2door;User Id=postgres;Password=12345;";
         public Door2DoorController(IConfiguration configuration)
         {
-            connectionString = Environment.GetEnvironmentVariable("foo");
-            Console.WriteLine("connection: " + connectionString);
+            roomFactory = new RoomFactory();
+            // connectionString = @"User id=postgres; Password=12345; Database" ;
+            // Console.WriteLine("connection: " + connectionString);
 
-            if (connectionString == null)
-                Console.WriteLine("No connectionstring");
+            // if (connectionString == null)
+                // Console.WriteLine("No connectionstring");
         }
 
         [HttpGet(Name = "GetRoute")]
@@ -25,12 +30,12 @@ namespace Door2Door_API.Controllers
             List<string> routeGroup = new List<string>();
             try
             {
-                using(NpgsqlConnection connection = new NpgsqlConnection(connectionString))
+                using(NpgsqlConnection connection = new NpgsqlConnection(CONNECTIONSTRING))
                 {
                     using (NpgsqlCommand pgSqlCommand = new NpgsqlCommand())
                     {
-                        pgSqlCommand.CommandText = "SELECT ST_AsGeoJSON(n.geom) from shitroute as n " +
-                                                    "join (select * from pgr_dijkstra('select id, source, target, cost from shitroute', 1, 12, false)) as route " +
+                        pgSqlCommand.CommandText = "SELECT ST_AsGeoJSON(n.geom) from network as n " +
+                                                    "join (select * from pgr_dijkstra('select id, source, target, distance as cost from network', 1, 12, false)) as route " +
                                                     "on n.id = route.edge";
                         pgSqlCommand.Connection = connection;
                         if (connection.State != ConnectionState.Open)
@@ -54,6 +59,27 @@ namespace Door2Door_API.Controllers
                 Console.WriteLine(e.Message);
             }
             return routeGroup;
+        }
+
+        [HttpGet("GetRoomById", Name = "GetRoom")]
+        public Room GetRoomById(int id)
+        {
+            using var connection = new NpgsqlConnection(CONNECTIONSTRING);
+            using var command = new NpgsqlCommand();
+            command.Connection = connection;
+            command.CommandText = $@"SELECT * FROM get_room_by_id({id})";
+            command.Connection.Open();
+            command.Prepare();
+            using var reader = command.ExecuteReader();
+            // return reader.ReadFirstOrDefault(r => roomFactory.Build(r));
+            // return reader.ReadFirstOrDefault
+            var rooms = new List<Room>();
+            while (reader.Read())
+            {
+                return roomFactory.Build(reader);
+            }
+
+            return null!;
         }
     }
 }
---
+++ Door2Door-API/DataReaderExtension.cs
@@ -0,0 +1,24 @@
+﻿using System.Data;
+using System.Diagnostics.CodeAnalysis;
+using Npgsql;
+
+namespace Door2Door_API;
+
+public static class DataReaderExtension
+{
+    public static T ReadFirstOrDefault<T>([NotNull] this IDataReader reader,
+        [NotNull] Func<IDataRecord, T> recordReader)
+    {
+        if (reader == null)
+        {
+            throw new ArgumentNullException(nameof(reader));
+        }
+
+        if (recordReader == null)
+        {
+            throw new ArgumentNullException(nameof(recordReader));
+        }
+
+        return (reader.Read() ? recordReader(reader) : default)!;
+    }
+}
