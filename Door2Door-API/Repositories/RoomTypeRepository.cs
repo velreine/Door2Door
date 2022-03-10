e80a1250cab2125cdb589c22dd1de56f97d5127d
Jonas Iversen <Joi>
2022-03-10 10:22:46 +0100

    Add roomtype entity to room and adjust factories and repos

--- Door2Door-API/Repositories/RoomTypeRepository.cs
+++ Door2Door-API/Repositories/RoomTypeRepository.cs
@@ -7,6 +7,10 @@ namespace Door2Door_API.Models;
 public class RoomTypeRepository : BaseRepository, IRoomTypeRepository
 {
     private readonly IFactory<RoomType> factory;
+    
+    // This is done because the factory.Build expects a specific name from the columns. 
+    private const string TABLE = "room_type rt";
+    private const string COLUMNS = "rt.id as room_type_id, rt.room_type as type";
 
     public RoomTypeRepository(IDbConnection connection, IFactory<RoomType> factory) : base(connection)
     {
@@ -16,7 +20,7 @@ public class RoomTypeRepository : BaseRepository, IRoomTypeRepository
     public async Task<IEnumerable<RoomType>> GetAllAsync()
     {
         IList<RoomType> types = new List<RoomType>();
-        const string query = "SELECT * FROM room_type";
+        const string query = $"SELECT {COLUMNS} FROM {TABLE}";
         using var reader = await Connection.ExecuteReaderAsync(query);
         while (reader.Read())
         {
@@ -28,7 +32,7 @@ public class RoomTypeRepository : BaseRepository, IRoomTypeRepository
 
     public async Task<RoomType> GetByIdAsync(long id)
     {
-        const string query = "SELECT * FROM room_type WHERE id = :type_id";
+        const string query = $"SELECT {COLUMNS} FROM {TABLE} WHERE id = :type_id";
         using var reader = await Connection.ExecuteReaderAsync(query, id);
         return reader.ReadFirstOrDefault(r => factory.Build(r));
     }
